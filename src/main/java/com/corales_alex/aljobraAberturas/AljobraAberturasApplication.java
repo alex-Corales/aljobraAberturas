a21fb803f87e49febca06c6d09cdfd0761e9d1ef
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 20:40:47 -0300

    Initial commit

---
+++ src/main/java/com/corales_alex/aljobraAberturas/AljobraAberturasApplication.java
@@ -0,0 +1,13 @@
+package com.corales_alex.aljobraAberturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AljobraAberturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AljobraAberturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corales_alex/aljobraAberturas/AljobraAberturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corales_alex.aljobraAberturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AljobraAberturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
