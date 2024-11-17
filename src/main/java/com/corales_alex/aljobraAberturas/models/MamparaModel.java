1d937a73ff983196ea6e0a06a71eafe622a81e7b
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 21:31:46 -0300

    Create IMamparaRepository.java

--- src/main/java/com/corales_alex/aljobraAberturas/models/MamparaModel.java
+++ src/main/java/com/corales_alex/aljobraAberturas/models/MamparaModel.java
@@ -1,6 +1,5 @@
 package com.corales_alex.aljobraAberturas.models;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
---
+++ src/main/java/com/corales_alex/aljobraAberturas/repositorys/IMamparaRepository.java
@@ -0,0 +1,9 @@
+package com.corales_alex.aljobraAberturas.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.corales_alex.aljobraAberturas.models.MamparaModel;
+
+public interface IMamparaRepository extends JpaRepository<MamparaModel, Long>{
+    
+}
