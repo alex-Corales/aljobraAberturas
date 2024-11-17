dbae3d09ff8e1a49416d192d95eeb42d4d4f7c8d
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 21:38:58 -0300

    Implemento metodos CRUD

--- src/main/java/com/corales_alex/aljobraAberturas/services/IMamparaService.java
+++ src/main/java/com/corales_alex/aljobraAberturas/services/IMamparaService.java
@@ -1,5 +1,13 @@
 package com.corales_alex.aljobraAberturas.services;
 
+import java.util.List;
+
+import com.corales_alex.aljobraAberturas.models.MamparaModel;
+
 public interface IMamparaService {
-    
+    public void saveMampara(MamparaModel mampara);
+    public List<MamparaModel> getAllMamparas();
+    public MamparaModel getMampara();
+    public void deteleMampara(Long idMampara);
+    public void updateMampara(Long idMampara, MamparaModel mamparaNueva);
 }
--- src/main/java/com/corales_alex/aljobraAberturas/services/MamparaService.java
+++ src/main/java/com/corales_alex/aljobraAberturas/services/MamparaService.java
@@ -1,8 +1,47 @@
 package com.corales_alex.aljobraAberturas.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.corales_alex.aljobraAberturas.models.MamparaModel;
+import com.corales_alex.aljobraAberturas.repositorys.IMamparaRepository;
+
 @Service
-public class MamparaService {
+public class MamparaService implements IMamparaService{
+
+    @Autowired
+    private IMamparaRepository iMamparaRepository;
+
+    @Override
+    public void saveMampara(MamparaModel mampara) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveMampara'");
+    }
+
+    @Override
+    public List<MamparaModel> getAllMamparas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllMamparas'");
+    }
+
+    @Override
+    public MamparaModel getMampara() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMampara'");
+    }
+
+    @Override
+    public void deteleMampara(Long idMampara) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deteleMampara'");
+    }
+
+    @Override
+    public void updateMampara(Long idMampara, MamparaModel mamparaNueva) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateMampara'");
+    }
     
 }
