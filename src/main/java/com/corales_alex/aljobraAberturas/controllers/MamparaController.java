e8c4b3deb009010cba389a5613fb1dc1b20184f9
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-17 00:14:04 -0300

    Anotation requestMapping

--- src/main/java/com/corales_alex/aljobraAberturas/controllers/MamparaController.java
+++ src/main/java/com/corales_alex/aljobraAberturas/controllers/MamparaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,35 +17,36 @@ import com.corales_alex.aljobraAberturas.models.MamparaModel;
 import com.corales_alex.aljobraAberturas.services.IMamparaService;
 
 @RestController
+@RequestMapping("/mamparas")
 public class MamparaController {
     
     @Autowired
     private IMamparaService iMamparaService;
 
-    @PostMapping("/mamparas/crear")
+    @PostMapping("/crear")
     public String saveMampara(@RequestBody MamparaModel mampara){
         iMamparaService.saveMampara(mampara);
         return "La mampara se guardo con exito";
     } 
 
-    @GetMapping("/mamparas")
+    @GetMapping("/")
     @ResponseBody
     public List<MamparaModel> getAllMamparas(){
         return iMamparaService.getAllMamparas();
     }
 
-    @GetMapping("/mamparas/{idMampara}")
+    @GetMapping("/{idMampara}")
     @ResponseBody
     public MamparaModel getMamparaModel(@PathVariable Long idMampara){
         return iMamparaService.getMampara(idMampara);
     }
 
-    @DeleteMapping("/mamparas/eliminar/{idMampara}")
+    @DeleteMapping("/eliminar/{idMampara}")
     public String deleteMampara(@PathVariable Long idMampara){
         return "La mampara se elimino con exito";
     }
 
-    @PutMapping("/mamparas/modificar/{idMampara}")
+    @PutMapping("/modificar/{idMampara}")
     public String updateMampara(@PathVariable Long idMampara, @RequestBody MamparaModel mamparaNueva){
         iMamparaService.updateMampara(idMampara, mamparaNueva);
         return "La mampara se modifico con exito";
