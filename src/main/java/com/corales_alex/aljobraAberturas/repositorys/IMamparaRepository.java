788f662d7ecc9bf0f4cb36f9ff3b25d6db6ac37c
Alex Nahuel Corales <alexcorales21@gmail.com>
2024-11-16 21:34:55 -0300

    Mapeo

--- src/main/java/com/corales_alex/aljobraAberturas/repositorys/IMamparaRepository.java
+++ src/main/java/com/corales_alex/aljobraAberturas/repositorys/IMamparaRepository.java
@@ -1,9 +1,11 @@
 package com.corales_alex.aljobraAberturas.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.corales_alex.aljobraAberturas.models.MamparaModel;
 
+@Repository
 public interface IMamparaRepository extends JpaRepository<MamparaModel, Long>{
-    
+
 }
--- src/main/java/com/corales_alex/aljobraAberturas/services/MamparaService.java
+++ src/main/java/com/corales_alex/aljobraAberturas/services/MamparaService.java
@@ -1,5 +1,8 @@
 package com.corales_alex.aljobraAberturas.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MamparaService {
     
 }
